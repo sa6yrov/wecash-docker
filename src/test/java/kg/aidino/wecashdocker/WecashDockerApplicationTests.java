91281601bc3e71077b00ffe204744d1336052e61
sa6yrov <sabyrov.aidin@gmail.com>
2024-03-04 20:18:58 +0600

    -test

--- src/test/java/kg/aidino/wecashdocker/WecashDockerApplicationTests.java
+++ src/test/java/kg/aidino/wecashdocker/WecashDockerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WecashDockerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
